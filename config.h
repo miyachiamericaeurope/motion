e919d4618eb71f37e062fef57c2d1142bf6806af
tmcolby <tyson.colby@innotech.engineering>
2019-07-02 08:51:14 -0700

    un-ignored all files

---
+++ config.h
@@ -0,0 +1,178 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if BKTR is around */
+/* #undef HAVE_BKTR */
+
+/* Define to 1 if you have the <dev/bktr/ioctl_bt848.h> header file. */
+/* #undef HAVE_DEV_BKTR_IOCTL_BT848_H */
+
+/* Define to 1 if you have the <dev/bktr/ioctl_meteor.h> header file. */
+/* #undef HAVE_DEV_BKTR_IOCTL_METEOR_H */
+
+/* Define to 1 if you have the <dev/ic/bt8xx.h> header file. */
+/* #undef HAVE_DEV_IC_BT8XX_H */
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if FFMPEG is around */
+#define HAVE_FFMPEG 1
+
+/* Define to 1 if libintl is around */
+#define HAVE_INTL 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <jerror.h> header file. */
+#define HAVE_JERROR_H 1
+
+/* Define to 1 if you have the <jpeglib.h> header file. */
+#define HAVE_JPEGLIB_H 1
+
+/* Define to 1 if you have the <libintl.h> header file. */
+#define HAVE_LIBINTL_H 1
+
+/* Define to 1 if you have the <linux/videodev2.h> header file. */
+#define HAVE_LINUX_VIDEODEV2_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <microhttpd.h> header file. */
+#define HAVE_MICROHTTPD_H 1
+
+/* Define to 1 if we want MMAL */
+#define HAVE_MMAL 1
+
+/* Define to 1 if you have MYSQL support */
+/* #undef HAVE_MYSQL */
+
+/* Define to 1 if you have PGSQL support */
+/* #undef HAVE_PGSQL */
+
+/* Define if you have pthread_getname_np function. */
+#define HAVE_PTHREAD_GETNAME_NP 1
+
+/* Define to 1 if you have the <pthread_np.h> header file. */
+/* #undef HAVE_PTHREAD_NP_H */
+
+/* Define if you have pthread_setname_np function. */
+#define HAVE_PTHREAD_SETNAME_NP 1
+
+/* Define to 1 if you have the <setjmp.h> header file. */
+#define HAVE_SETJMP_H 1
+
+/* Define to 1 if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H 1
+
+/* Define to 1 if you have SQLITE3 shared library support */
+/* #undef HAVE_SQLITE3 */
+
+/* Define to 1 if you have SQLITE3 embedded support */
+/* #undef HAVE_SQLITE3_EMBEDDED */
+
+/* Define to 1 if you have the <sqlite3.h> header file. */
+/* #undef HAVE_SQLITE3_H */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/mman.h> header file. */
+#define HAVE_SYS_MMAN_H 1
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <time.h> header file. */
+#define HAVE_TIME_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if V4L2 is around */
+#define HAVE_V4L2 1
+
+/* Define to 1 if WEBP is around */
+/* #undef HAVE_WEBP */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "motion"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "motion 4.2.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "motion"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "4.2.2"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Enable extensions on AIX 3, Interix.  */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+/* Enable threading extensions on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+
+
+/* Define if you have XSI strerror_r function. */
+/* #undef XSI_STRERROR_R */
+
+/* Define to 1 if on MINIX. */
+/* #undef _MINIX */
+
+/* Define to 2 if the system does not provide POSIX.1 features except with
+   this defined. */
+/* #undef _POSIX_1_SOURCE */
+
+/* Define to 1 if you need to in order for `stat' and other things to work. */
+/* #undef _POSIX_SOURCE */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
