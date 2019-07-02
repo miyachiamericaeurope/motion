87b817974e16bebefa2778792ad463cccfeb2552
tmcolby <tyson.colby@innotech.engineering>
2019-07-02 15:04:10 -0700

    modified motion sysv init script and installer.

--- pi_motion/install.sh
+++ pi_motion/install.sh
@@ -2,5 +2,7 @@
 yes N | sudo dpkg -i pi_stretch_motion_4.2.2-1_armhf.deb
 sudo apt-get install -f
 sudo cp motion.conf /etc/motion/
-sudo cp motion /etc/default
+sudo cp motion.start_motion_daemon /etc/default/motion
+sudo cp motion.initd /etc/init.d/motion
+sudo systemctl daemon-reload
 sudo systemctl restart motion
